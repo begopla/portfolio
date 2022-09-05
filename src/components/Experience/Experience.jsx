bd2acd02272a99c568c1a7deec85488f2f260522
begopla <begonapr@gmail.com>
2022-09-05 15:40:56 +0400

    experience data and layout

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -6,6 +6,8 @@ import {Flex,
         useColorModeValue
       } from '@chakra-ui/react';
 import { HashLink } from 'react-router-hash-link';
+import CV from '../../assets/CV Begona Pla Rubio .pdf'
+import experienceData from "../../data/experience.json";
 const Experience = () => {
   return (
     <section id='experience'>
@@ -15,9 +17,11 @@ const Experience = () => {
       EXPERIENCE
     </Box>
     
-    <Text>Hello</Text> 
+    <Flex >
 
-    <Button bg={useColorModeValue('blue.200', 'gray.900')} width ='20vw' mt='5vh'  ><HashLink to="#top">ABOUT</HashLink> </Button>
+    <Button bg={useColorModeValue('blue.200', 'gray.900')} width ='20vw' mt='5vh'  > <a href={CV} download> Download complete CV </a></Button>
+    <Button bg={useColorModeValue('blue.200', 'gray.900')} width ='20vw' mt='5vh' ml='5vw' ><HashLink to="#top">Go up</HashLink> </Button>
+    </Flex>
     </Flex>
     </section>
   )
