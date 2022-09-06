4ba25909956a311cca30719fd036e38f8349c9db
begopla <begonapr@gmail.com>
2022-09-06 17:28:49 +0400

    Project card finished

--- src/components/OneProject/Oneproject.jsx
+++ src/components/OneProject/Oneproject.jsx
@@ -2,44 +2,87 @@ import React from "react";
 import {
   Box,
   Flex,
-  Button,
-  Icon,
+  IconButton,
   Image,
   Text,
   useColorModeValue,
-  Link
+  Link,
 } from "@chakra-ui/react";
-import './Onproject.css';
-const Oneproject = ({name, image, github_url,website_url, description,technologies}) => {
+import "./Onproject.css";
+import { FaGithub } from "react-icons/fa";
+import { MdLaptop } from "react-icons/md";
+const Oneproject = ({
+  name,
+  image,
+  github_url,
+  website_url,
+  description,
+  technologies,
+}) => {
   let boxBg = useColorModeValue("gray.100", "#111c44");
   let secondaryBg = useColorModeValue("gray.50", "whiteAlpha.100");
   let mainText = useColorModeValue("gray.800", "white");
   let iconBox = useColorModeValue("gray.100", "whiteAlpha.200");
   let iconColor = useColorModeValue("brand.200", "white");
   return (
-    <Box m='2rem 1rem 2rem 1rem'
-          >
+    <Box m="2rem 1rem 2rem 1rem">
       <Flex
         borderWith={2}
-        borderRadius='20px'
-        border='solid #e2e8f0'
+        borderRadius="20px"
+        border="solid #e2e8f0"
         bg={boxBg}
         h="60vh"
         w={{ base: "310px", md: "345px" }}
         direction="column"
       >
-      <Link href={website_url} isExternal>
-        <Box >
-          <Flex w="100%" mb="3px" p="10px 30px" >
-            <Image src={image} maxHeight='18vh'/>
-          </Flex>
           <Box>
-            <Text fontWeight="600" color={mainText} w="100%" fontSize="2xl" pl='20px' mb='0.5vh' >
-              {name}
-            </Text>
+            <Flex w="100%" mb="3px" p="10px 30px">
+               <Link href={website_url} isExternal>
+                 <Image src={image} maxHeight="18vh" />
+               </Link>
+            </Flex>
+            <Flex >
+            <Box>
+              <Text
+                fontWeight="600"
+                color={mainText}
+                w="100%"
+                fontSize="2xl"
+                pl="20px"
+                mb="0.5vh"
+              >
+                {name}
+              </Text>
+            </Box>
+            <Box>
+            <IconButton
+                    as={Link}
+                    ml='8vw'
+                    href={github_url}
+                    size={"md"}
+                    icon={<FaGithub />}
+                    aria-label={"project directory"}
+                    _hover={{
+                      textDecoration: "none",
+                      
+                    }}
+                  />
+            </Box>
+            <Box>
+            <IconButton
+                    as={Link}
+                    href={website_url}
+                    size={"md"}
+                    icon={<MdLaptop />}
+                    aria-label={"project website "}
+                    _hover={{
+                      textDecoration: "none",
+                    }}
+                  />
+            </Box>
+            </Flex>
           </Box>
-        </Box>
-        </Link>
+       
         <Flex
           bg={secondaryBg}
           w="100%"
@@ -57,11 +100,15 @@ const Oneproject = ({name, image, github_url,website_url, description,technologi
             fontWeight="500"
             mb="auto"
           >
-           {description}
+            {description}
           </Text>
           <div className="technologies-container">
             {technologies.map((eachTechnology) => {
-              return <div className="technologies">{eachTechnology}</div>;
+              return (
+                <div className="technologies" key={eachTechnology}>
+                  {eachTechnology}
+                </div>
+              );
             })}
           </div>
         </Flex>
